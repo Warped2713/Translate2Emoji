bcc627b352dc7590c21b995fe998a9e831da31cd
Natalie Diebold <Warped2713@users.noreply.github.com>
2017-11-03 16:09:33 -0400

    Update translate.js

--- js/translate.js
+++ js/translate.js
@@ -1,11 +1,13 @@
 let prefix = ':';
 let suffix = ':';
 let source = '';
+let charset = 'abcdefghijklmnopqstuvwxyz1234567890!?';
 
 document.getElementById('source').addEventListener('change', function() {
   source = document.getElementById('source').value;
   let output = '';
   for (let c=0; c < source.length; c++) {
+    // TODO: Check if c is in charset
     if (source[c].trim() == "") {
         output += " ";
     } else {
@@ -23,4 +25,8 @@ document.getElementById('pre').addEventListener('change', function() {
 
 document.getElementById('post').addEventListener('change', function() {
   suffix = document.getElementById('post').value;
+});
+
+document.getElementById('charset').addEventListener('change', function() {
+  charset = document.getElementById('charset').value;
 });
