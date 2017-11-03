f5463b9ed57e3a62d763cb4a0e769228e2a17d87
Natalie Diebold <Warped2713@users.noreply.github.com>
2017-11-03 15:34:49 -0400

    Create translate.js

---
+++ js/translate.js
@@ -0,0 +1,22 @@
+let prefix = ':';
+let suffix = ':';
+let source = '';
+
+document.getElementById('source').addEventListener('change', function() {
+  source = document.getElementById('source').value;
+  let output = '';
+  for (let c=0; c < source.length; c++) {
+    output += prefix + source[c] + suffix;
+    console.log(source[c]);
+  }
+  console.log(output);
+  document.getElementById('output').value = output;
+});
+
+document.getElementById('pre').addEventListener('change', function() {
+  prefix = document.getElementById('pre').value;
+});
+
+document.getElementById('post').addEventListener('change', function() {
+  suffix = document.getElementById('post').value;
+});
